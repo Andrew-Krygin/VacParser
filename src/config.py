e6aa085d76a412be7b6216a85982102ab1adb886
Andrew Krygin <andrew_krygin@protonmail.com>
2025-07-29 14:01:51 +0300

    feat(src/config): add config module
    - config: base directory and data folder path
    - constants: VALID_SAVE_CHOICES and VALID_FILE_CHOICES sets

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+FILE_PATH = os.path.join(BASE_DIR, "data")
+
+VALID_SAVE_CHOICES = {"1", "2", "3", "4", "5"}
+VALID_FILE_CHOICES = {"1", "2", "3"}
